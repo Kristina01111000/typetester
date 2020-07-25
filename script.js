4cfc6baf4ef567028ac1fd76882c6a93e8a86182
Kristina01111000 <64657260+Kristina01111000@users.noreply.github.com>
2020-07-25 19:25:47 -0400

    added leading zero function

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 var timer = [0,0,0,0};
 var interval;
 var timerRunning = false;
+
+// leading zero in front of timer
+function leadingZero(time){
+  if(time <= 9){
+    time = "0" + time;
+  }
+  return time;
+}
