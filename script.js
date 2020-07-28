8c8fa8c2d56a2b3d0b3d9fc53ef528f37def077b
Kristina01111000 <64657260+Kristina01111000@users.noreply.github.com>
2020-07-28 10:12:24 -0400

    added test wrapper const

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const theTimer = document.querySelector('.timer'); 
 const testArea = document.querySelector('#test-area');
-
+const testWrapper = document.querySelector('#test-wrapper');
 var timer = [0,0,0,0};
 var interval;
 var timerRunning = false;
